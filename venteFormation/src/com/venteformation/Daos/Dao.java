2c2d17fea6ffedbc48efbe6e4e47716ccb018521
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-05 14:09:19 +0100

    Création de l'interface Dao

---
+++ venteFormation/src/com/venteformation/Daos/Dao.java
@@ -0,0 +1,17 @@
+package com.venteformation.Daos;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    void create(T obj);
+
+    void update(T obj);
+
+    void delete(int id);
+
+    T findById(int id);
+
+    List<T> findAll();
+}
+
