c752065875e916cb810a5e5a9cefbd2723f227aa
DucasseRafael91 <rafael.ducasse@fms-ea.com>
2026-01-06 13:51:15 +0100

    Add method to find formations by category

--- src/com/venteformation/Daos/FormationDao.java
+++ src/com/venteformation/Daos/FormationDao.java
@@ -64,4 +64,38 @@ public class FormationDao implements Dao<Formation> {
 
         return formations;
     }
+
+    public ArrayList<Formation> findByCategory(Category category) {
+        ArrayList<Formation> formations = new ArrayList<>();
+
+        String sql =
+                "SELECT f_nom, f_description, duree_jours, prix " +
+                        "FROM v_formation f " +
+                        "INNER JOIN v_categorie c ON f.f_fk_categorie = c.c_id " +
+                        "WHERE c.c_nom = ?";
+
+        try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setString(1, category.getName());
+
+
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("f_nom");
+                String description = resultSet.getString("f_description");
+                int days_amount = resultSet.getInt("duree_jours");
+                double price = resultSet.getDouble("prix");
+
+                formations.add(new Formation(name, description, days_amount, price, category));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return formations;
+    }
+
 }
