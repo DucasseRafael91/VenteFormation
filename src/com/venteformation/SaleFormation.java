609a16ab8e24bd25c12938c77d9a5fe3982fc46a
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-06 15:06:44 +0100

    Ajout d'un second menu

--- src/com/venteformation/SaleFormation.java
+++ src/com/venteformation/SaleFormation.java
@@ -13,21 +13,63 @@ public class SaleFormation {
         FormationDao dao = new FormationDao();
         Scanner scanner = new Scanner(System.in);
 
-        while (true) { 
+        while (true) {
             System.out.println("Voulez vous vous connectez ? (o/n)");
             System.out.print("Votre choix : ");
 
-            String choix = scanner.next();
+            String choice = scanner.next();
 
-            if (choix == "o") {
+            if (choice.equals("o")) {
                 System.out.println("Au revoir !");
-                break; 
+                break;
 
-            } else if (choix == "n") {
+            } else if (choice.equals("n")) {
 
-                ArrayList<Formation> formations = dao.findAll();
-                for (Formation formation : formations) {
-                    System.out.println(formation);
+                boolean exitMenu = false;
+
+                while (!exitMenu) {
+                    System.out.println("\n=== Menu Non Connecté ===");
+                    System.out.println("1. Afficher toutes les formations");
+                    System.out.println("2. Afficher les formations par catégorie");
+                    System.out.println("3. Rechercher les formations par mot clé");
+                    System.out.println("4. Afficher les formations en présentiel");
+                    System.out.println("5. Afficher les formations en distanciel");
+                    System.out.println("6. Retour au menu précédent");
+                    System.out.print("Votre choix : ");
+
+                    int menuChoice = scanner.nextInt();
+                    scanner.nextLine(); // Consommer le retour à la ligne
+
+                    switch (menuChoice) {
+                        case 1:
+                            System.out.println("Afficher toutes les formations...");
+                            ArrayList<Formation> formations = dao.findAll();
+                            for (Formation formation : formations) {
+                                System.out.println(formation);
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Afficher les formations par catégorie...");
+                            // TODO: filtrer par catégorie
+                            break;
+                        case 3:
+                            System.out.println("Rechercher les formations par mot clé...");
+                            // TODO: filtrer par mot clé
+                            break;
+                        case 4:
+                            System.out.println("Afficher les formations en présentiel...");
+                            // TODO: filtrer par présentiel
+                            break;
+                        case 5:
+                            System.out.println("Afficher les formations en distanciel...");
+                            // TODO: filtrer par distanciel
+                            break;
+                        case 6:
+                            exitMenu = true; // revenir au menu principal
+                            break;
+                        default:
+                            System.out.println("Choix invalide, veuillez réessayer.");
+                    }
                 }
 
             } else {
