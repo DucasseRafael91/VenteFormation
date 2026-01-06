f3b0814c27a1a33f8e6bf6bc3822baf323c79c30
DucasseRafael91 <rafael.ducasse@fms-ea.com>
2026-01-06 13:31:29 +0100

    Ajout d'un menu dans SaleFormation.java
    Added a menu for user interaction to display formations or exit.

--- src/com/venteformation/SaleFormation.java
+++ src/com/venteformation/SaleFormation.java
@@ -4,16 +4,36 @@ import com.venteformation.Daos.FormationDao;
 import com.venteformation.Entities.Formation;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SaleFormation {
 
     public static void main(String[] args) {
 
         FormationDao dao = new FormationDao();
-        ArrayList<Formation> formations = dao.findAll();
+        Scanner scanner = new Scanner(System.in);
 
-        for (Formation formation : formations) {
-            System.out.println(formation);
+        while (true) { // boucle infinie
+            System.out.println("\n--- Menu ---");
+            System.out.println("1 : Afficher les formations");
+            System.out.println("2 : Quitter");
+            System.out.print("Votre choix : ");
+
+            int choix = scanner.nextInt();
+
+            if (choix == 1) {
+                ArrayList<Formation> formations = dao.findAll();
+                for (Formation formation : formations) {
+                    System.out.println(formation);
+                }
+            } else if (choix == 2) {
+                System.out.println("Au revoir !");
+                break; // quitte la boucle et le programme
+            } else {
+                System.out.println("Choix invalide, veuillez réessayer.");
+            }
         }
+
+        scanner.close();
     }
 }
