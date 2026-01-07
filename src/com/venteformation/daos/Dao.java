b44bc893edb455f19d579a65a6dbdc2ab51f4dfc
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-07 13:58:58 +0100

    Ajout de javaDoc dans Dao

--- src/com/venteformation/daos/Dao.java
+++ src/com/venteformation/daos/Dao.java
@@ -4,14 +4,40 @@ import java.util.List;
 
 public interface Dao<T> {
 
+    /**
+     * Méthode pour créer un objet
+     * dans la base de données.
+     * @param obj :objet
+     */
     void create(T obj);
 
+    /**
+     * Méthode pour mettre à jour un objet
+     * dans la base de données.
+     * @param obj :objet
+     */
     void update(T obj);
 
+    /**
+     * Méthode pour supprimer un objet
+     * dans la base de données.
+     * @param id :id
+     */
     void delete(int id);
 
+    /**
+     * Méthode pour trouver un objet par id
+     * dans la base de données.
+     * @param id :id
+     * @return : objet à retourner
+     */
     T findById(int id);
 
+    /**
+     * Méthode pour trouver tous les objets
+     * dans la base de données.
+     * @return : objets à retourner
+     */
     List<T> findAll();
 }
 
