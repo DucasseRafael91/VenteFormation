af418d6004d4c4af746be530a2335e4ca8c89bdc
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-07 13:55:31 +0100

    Ajout de javaDoc dans ClientDao

--- src/com/venteformation/daos/ClientDao.java
+++ src/com/venteformation/daos/ClientDao.java
@@ -3,7 +3,12 @@ package com.venteformation.daos;
 import com.venteformation.Entities.Client;
 import com.venteformation.Entities.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import java.util.ArrayList;
 
 public class ClientDao implements Dao<Client> {
@@ -13,39 +18,53 @@ public class ClientDao implements Dao<Client> {
     private static final String PASSWORD = "";
 
     @Override
-    public void create(Client client) {
+    public void create(final Client client) {
         // À implémenter plus tard
     }
 
     @Override
-    public void update(Client client) {
+    public void update(final Client client) {
         // À implémenter plus tard
     }
 
     @Override
-    public void delete(int id) {
+    public void delete(final int id) {
         // À implémenter plus tard
     }
 
+    /**
+     * Méthode pour trouver les clients avec l'id
+     * dans la base de données.
+     */
     @Override
-    public Client findById(int id) {
+    public Client findById(final int id) {
         // À implémenter plus tard
         return null;
     }
 
+    /**
+     * Méthode pour trouver tous les clients
+     * dans la base de données.
+     */
     @Override
     public ArrayList<Client> findAll() {
         return new ArrayList<>();
     }
 
-    public ArrayList<Client> findByUser(User user) {
+    /**
+     * Méthode pour trouver les clients d'un utilisateur
+     * dans la base de données.
+     * @param user : utilisateur
+     * @return clients : clients de l'utilisateur
+     */
+    public ArrayList<Client> findByUser(final User user) {
         ArrayList<Client> clients = new ArrayList<>();
 
         String sql =
-                "SELECT c_nom, c_prenom, c_email, c_adresse, c_telephone, c_id " +
-                        "FROM v_client " +
-                        "INNER JOIN v_utilisateur ON c_fk_utilisateur = u_id " +
-                        "WHERE u_identifiant = ?";
+                "SELECT c_nom, c_prenom, c_email, c_adresse, c_telephone, c_id "
+                        + "FROM v_client "
+                        + "INNER JOIN v_utilisateur ON c_fk_utilisateur = u_id "
+                        + "WHERE u_identifiant = ?";
 
         try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
@@ -54,12 +73,12 @@ public class ClientDao implements Dao<Client> {
 
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 while (resultSet.next()) {
-                    String lastName = resultSet.getString(1);
-                    String firstName = resultSet.getString(2);
-                    String mail = resultSet.getString(3);
-                    String address = resultSet.getString(4);
-                    String phone = resultSet.getString(5);
-                    Integer id = resultSet.getInt(6);
+                    String lastName = resultSet.getString("c_nom");
+                    String firstName = resultSet.getString("c_prenom");
+                    String mail = resultSet.getString("c_email");
+                    String address = resultSet.getString("c_adresse");
+                    String phone = resultSet.getString("c_telephone");
+                    Integer id = resultSet.getInt("c_id");
 
                     clients.add(new Client(
                             id,
