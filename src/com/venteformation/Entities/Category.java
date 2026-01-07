86501b578ffb5f0c9b73ba8b60b720148bc8ff41
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-07 15:43:51 +0100

    Ajout de javaDoc dans Category

--- src/com/venteformation/Entities/Category.java
+++ src/com/venteformation/Entities/Category.java
@@ -8,29 +8,45 @@ public class Category {
 
 
     // Constructeur
-    public Category(String name) {
+    public Category(final String name) {
         this.name = name;
     }
 
+    /**
+     * Méthode toString.
+     */
     @Override
     public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return "Category{"
+                + "id=" + id
+                + ", name='"
+                + name
+                + '\''
+                + '}';
     }
 
     // Getters
+    /**
+     * Méthode getId.
+     * @return : id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Méthode getName.
+     * @return : nom
+     */
     public String getName() {
         return name;
     }
 
-    // Setters
-    public void setId(int id) {
+    /**
+     * Méthode setId.
+     * @param id : id
+     */
+    public void setId(final int id) {
         this.id = id;
     }
 
