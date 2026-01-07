bded604ee9c67ec14a5ae823f36acee7b217f534
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-07 10:51:20 +0100

    Update de orderDao pour rajouter les lignes de commande

--- src/com/venteformation/daos/OrderDao.java
+++ src/com/venteformation/daos/OrderDao.java
@@ -1,6 +1,7 @@
 package com.venteformation.daos;
 
 import com.venteformation.Entities.Client;
+import com.venteformation.Entities.Formation;
 import com.venteformation.Entities.Order;
 import com.venteformation.Entities.User;
 
@@ -14,20 +15,67 @@ public class OrderDao implements Dao<Order> {
     private static final String LOGIN = "root";
     private static final String PASSWORD = "";
 
-    @Override
-    public void create(Order order) {
-        String sql = "INSERT INTO v_commande (c_date, c_fk_client) VALUES (CURRENT_TIMESTAMP, ?)";
 
-        try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-             PreparedStatement ps = connection.prepareStatement(sql)) {
+    public void create(Order order, ArrayList<Formation> formations) {
+        String sqlCommande =
+                "INSERT INTO v_commande (c_date, c_fk_client) VALUES (CURRENT_TIMESTAMP, ?)";
+        String sqlLigne =
+                "INSERT INTO v_ligne_commande (l_fk_formation, l_fk_commande) VALUES (?, ?)";
+        String sqlSelectFormation =
+                "SELECT f_id FROM v_formation WHERE f_nom = ?";
 
-            ps.setInt(1, order.getClient().getId());
-            
+        try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD)) {
+
+            connection.setAutoCommit(false);
+
+            // 1️⃣ Insérer la commande
+            try (PreparedStatement psCommande =
+                         connection.prepareStatement(sqlCommande, Statement.RETURN_GENERATED_KEYS)) {
+
+                psCommande.setInt(1, order.getClient().getId());
+                psCommande.executeUpdate();
+
+                try (ResultSet rs = psCommande.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        order.setId(rs.getInt(1));
+                    }
+                }
+            }
+
+            // 2️⃣ Préparer les statements pour récupérer les IDs de formation et insérer les lignes
+            try (PreparedStatement psSelectFormation = connection.prepareStatement(sqlSelectFormation);
+                 PreparedStatement psLigne = connection.prepareStatement(sqlLigne)) {
+
+                for (Formation f : formations) {
+                    // Récupérer l'ID de la formation à partir du nom
+                    psSelectFormation.setString(1, f.getName());
+                    try (ResultSet rs = psSelectFormation.executeQuery()) {
+                        if (rs.next()) {
+                            int formationId = rs.getInt("f_id");
+
+                            // Insérer la ligne de commande avec l'ID correct
+                            psLigne.setInt(1, formationId);
+                            psLigne.setInt(2, order.getId());
+                            psLigne.executeUpdate();
+                        } else {
+                            System.out.println("La formation '" + f.getName() + "' n'existe pas en base !");
+                        }
+                    }
+                }
+            }
+
+            connection.commit();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+
+    @Override
+    public void create(Order obj) {
+
+    }
 
     @Override
     public void update(Order order) {
