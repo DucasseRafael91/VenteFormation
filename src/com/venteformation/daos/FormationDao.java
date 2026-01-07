a80bc4385a980820d3827934ea80836174d5089b
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-07 08:45:33 +0100

    Refactor de Formation Dao

--- src/com/venteformation/daos/FormationDao.java
+++ src/com/venteformation/daos/FormationDao.java
@@ -38,11 +38,13 @@ public class FormationDao implements Dao<Formation> {
     public ArrayList<Formation> findAll() {
         ArrayList<Formation> formations = new ArrayList<>();
 
-        String sql = "SELECT f_nom,f_description,duree_jours,prix,c_nom\n" +
-                "FROM v_formation\n" +
-                "INNER JOIN v_categorie\n" +
-                "ON v_formation.f_fk_categorie = v_categorie.c_id\n" +
-                "ORDER BY c_nom;";
+        String sql = """
+                SELECT f_nom, f_description, duree_jours, prix, c_nom
+                FROM v_formation
+                INNER JOIN v_categorie
+                    ON v_formation.f_fk_categorie = v_categorie.c_id
+                ORDER BY c_nom;
+                """;
 
         try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
              Statement statement = connection.createStatement();
@@ -51,12 +53,11 @@ public class FormationDao implements Dao<Formation> {
             while (resultSet.next()) {
                 String name = resultSet.getString(1);
                 String description = resultSet.getString(2);
-                Integer days_amount = resultSet.getInt(3);
+                Integer daysAmount = resultSet.getInt(3);
                 Double price = resultSet.getDouble(4);
 
                 Category category = new Category(resultSet.getString(5));
-
-                formations.add(new Formation(name, description, days_amount, price, category));
+                formations.add(new Formation(name, description, daysAmount, price, category));
             }
 
         } catch (SQLException e) {
@@ -69,27 +70,27 @@ public class FormationDao implements Dao<Formation> {
     public ArrayList<Formation> findByCategory(Category category) {
         ArrayList<Formation> formations = new ArrayList<>();
 
-        String sql =
-                "SELECT f_nom, f_description, duree_jours, prix " +
-                        "FROM v_formation f " +
-                        "INNER JOIN v_categorie c ON f.f_fk_categorie = c.c_id " +
-                        "WHERE c.c_nom = ?";
+        String sql = """
+                SELECT f_nom, f_description, duree_jours, prix
+                FROM v_formation f
+                INNER JOIN v_categorie c
+                    ON f.f_fk_categorie = c.c_id
+                WHERE c.c_nom = ?
+                """;
 
         try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
              PreparedStatement ps = connection.prepareStatement(sql)) {
 
             ps.setString(1, category.getName());
-
-
             ResultSet resultSet = ps.executeQuery();
 
             while (resultSet.next()) {
-                String name = resultSet.getString("f_nom");
-                String description = resultSet.getString("f_description");
-                int days_amount = resultSet.getInt("duree_jours");
-                double price = resultSet.getDouble("prix");
+                String name = resultSet.getString(1);
+                String description = resultSet.getString(2);
+                int daysAmount = resultSet.getInt(3);
+                double price = resultSet.getDouble(4);
 
-                formations.add(new Formation(name, description, days_amount, price, category));
+                formations.add(new Formation(name, description, daysAmount, price, category));
             }
 
         } catch (SQLException e) {
@@ -102,67 +103,63 @@ public class FormationDao implements Dao<Formation> {
     public ArrayList<Formation> findByType(Formation_type type) {
         ArrayList<Formation> formations = new ArrayList<>();
 
-        String sql =
-                "SELECT f_nom, f_description, duree_jours, prix, c_nom \n" +
-                        "FROM `v_formation` \n" +
-                        "INNER JOIN est_de_type\n" +
-                        "ON `f_id` = e_fk_formation \n" +
-                        "INNER JOIN v_type_formation\n" +
-                        "ON t_id = e_fk_type_formation\n" +
-                        "INNER JOIN v_categorie\n" +
-                        "ON v_formation.f_fk_categorie = v_categorie.c_id\n" +
-                        "WHERE t_nom = ?;";
+        String sql = """
+                SELECT f_nom, f_description, duree_jours, prix, c_nom
+                FROM v_formation
+                INNER JOIN est_de_type
+                    ON f_id = e_fk_formation
+                INNER JOIN v_type_formation
+                    ON t_id = e_fk_type_formation
+                INNER JOIN v_categorie
+                    ON v_formation.f_fk_categorie = v_categorie.c_id
+                WHERE t_nom = ?;
+                """;
 
         try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
              PreparedStatement ps = connection.prepareStatement(sql)) {
 
             ps.setString(1, type.getName());
-
             ResultSet resultSet = ps.executeQuery();
 
-            while (resultSet.next()) {
-                String name = resultSet.getString("f_nom");
-                String description = resultSet.getString("f_description");
-                int days_amount = resultSet.getInt("duree_jours");
-                double price = resultSet.getDouble("prix");
-
-                Category category = new Category(resultSet.getString(5));
-
-                formations.add(new Formation(name, description, days_amount, price, category));
-            }
+            findResult(formations, resultSet);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return formations;
+    }
+
+    private void findResult(ArrayList<Formation> formations, ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            String name = resultSet.getString(1);
+            String description = resultSet.getString(2);
+            int daysAmount = resultSet.getInt(3);
+            double price = resultSet.getDouble(4);
+
+            Category category = new Category(resultSet.getString(5));
+            formations.add(new Formation(name, description, daysAmount, price, category));
+        }
     }
 
     public ArrayList<Formation> findByKeyword(String keyword) {
         ArrayList<Formation> formations = new ArrayList<>();
 
-        String sql =
-                "SELECT f.f_nom, f.f_description, f.duree_jours, f.prix, c.c_nom " +
-                        "FROM v_formation f " +
-                        "INNER JOIN v_categorie c ON f.f_fk_categorie = c.c_id " +
-                        "WHERE f.f_nom LIKE ?";
+        String sql = """
+                SELECT f.f_nom, f.f_description, f.duree_jours, f.prix, c.c_nom
+                FROM v_formation f
+                INNER JOIN v_categorie c
+                    ON f.f_fk_categorie = c.c_id
+                WHERE f.f_nom LIKE ?
+                """;
 
         try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
              PreparedStatement ps = connection.prepareStatement(sql)) {
 
-            ps.setString(1, "%" + keyword + "%"); // Le % permet de chercher n'importe où dans le nom
-
+            ps.setString(1, "%" + keyword + "%");
             ResultSet resultSet = ps.executeQuery();
 
-            while (resultSet.next()) {
-                String name = resultSet.getString("f_nom");
-                String description = resultSet.getString("f_description");
-                int daysAmount = resultSet.getInt("duree_jours");
-                double price = resultSet.getDouble("prix");
-                Category category = new Category(resultSet.getString("c_nom"));
-
-                formations.add(new Formation(name, description, daysAmount, price, category));
-            }
+            findResult(formations, resultSet);
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -170,6 +167,4 @@ public class FormationDao implements Dao<Formation> {
 
         return formations;
     }
-
-
 }
