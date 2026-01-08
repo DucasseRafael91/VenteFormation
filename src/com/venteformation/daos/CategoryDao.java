9be715aaa0688397716a3a214a02b3e85e38bcf4
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-08 10:57:26 +0100

    Refactor de CategoryDao

--- src/com/venteformation/daos/CategoryDao.java
+++ src/com/venteformation/daos/CategoryDao.java
@@ -8,66 +8,66 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+/**
+ * Dao CategoryDao.
+ */
 public class CategoryDao implements Dao<Category> {
 
-    /**
-     * DAO permettant la gestion des catégories
-     * dans la base de données.
-     */
+  /**
+   * DAO permettant la gestion des catégories dans la base de données.
+   */
 
-    private static final String URL = "jdbc:mariadb://localhost:3307/formation";
-    private static final String LOGIN = "formation";
-    private static final String PASSWORD = "formation";
+  private static final String URL = "jdbc:mariadb://localhost:3307/formation";
+  private static final String LOGIN = "formation";
+  private static final String PASSWORD = "formation";
 
-    @Override
-    public void create(final Category category) {
-        // À implémenter plus tard
+  @Override
+  public void create(final Category category) {
+    // À implémenter plus tard
+  }
+
+  @Override
+  public void update(final Category category) {
+    // À implémenter plus tard
+  }
+
+  @Override
+  public void delete(final int id) {
+    // À implémenter plus tard
+  }
+
+  /**
+   * Méthode pour trouver une categorie par id dans la base de données.
+   */
+  @Override
+  public Category findById(final int id) {
+    // À implémenter plus tard
+    return null;
+  }
+
+
+  /**
+   * Méthode pour trouver toute les categories dans la base de données.
+   */
+  @Override
+  public ArrayList<Category> findAll() {
+    ArrayList<Category> categories = new ArrayList<>();
+
+    String sql = "SELECT c_nom FROM v_categorie";
+
+    try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql)) {
+
+      while (resultSet.next()) {
+        String name = resultSet.getString(1);
+        categories.add(new Category(name));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    @Override
-    public void update(final Category category) {
-        // À implémenter plus tard
-    }
-
-    @Override
-    public void delete(final int id) {
-        // À implémenter plus tard
-    }
-
-    /**
-     * Méthode pour trouver une categorie par id
-     * dans la base de données.
-     */
-    @Override
-    public Category findById(final int id) {
-        // À implémenter plus tard
-        return null;
-    }
-
-
-    /**
-     * Méthode pour trouver toute les categories
-     * dans la base de données.
-     */
-    @Override
-    public ArrayList<Category> findAll() {
-        ArrayList<Category> categories = new ArrayList<>();
-
-        String sql = "SELECT c_nom FROM v_categorie";
-
-        try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
-
-            while (resultSet.next()) {
-                String name = resultSet.getString(1);
-                categories.add(new Category(name));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return categories;
-    }
+    return categories;
+  }
 }
