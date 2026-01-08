626da576e8428b9481644ef46e520c162464200c
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-08 08:37:01 +0100

    Refactor de Client.java

--- src/com/venteformation/Entities/Client.java
+++ src/com/venteformation/Entities/Client.java
@@ -2,41 +2,43 @@ package com.venteformation.Entities;
 
 public class Client {
 
-    private int id;
-    private final String lastName;
-    private final String firstName;
-    private final String mail;
-    private final String address;
-    private final String phoneNumber;
-    private final User user;
+  private final String lastName;
+  private final String firstName;
+  private final String mail;
+  private final String address;
+  private final String phoneNumber;
+  private final User user;
+  private int id;
 
 
-    // Constructeur
-    public Client(final Integer id, final String lastName, final String firstName, final String mail, final String address, final String phoneNumber, final User user) {
-        this.id = id;
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.mail = mail;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.user = user;
-    }
+  // Constructeur
+  public Client(final Integer id, final String lastName, final String firstName, final String mail,
+      final String address, final String phoneNumber, final User user) {
+    this.id = id;
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.mail = mail;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.user = user;
+  }
 
-    @Override
-    public String toString() {
-        return lastName + " " + firstName + " | Email: " + mail + " | Téléphone: " + phoneNumber + " | Adresse: " + address;
-    }
-
-
-    // Getters
-    public int getId() {
-        return id;
-    }
+  @Override
+  public String toString() {
+    return lastName + " " + firstName + " | Email: " + mail + " | Téléphone: " + phoneNumber
+        + " | Adresse: " + address;
+  }
 
 
-    // Setters
-    public void setId(int id) {
-        this.id = id;
-    }
+  // Getters
+  public int getId() {
+    return id;
+  }
+
+
+  // Setters
+  public void setId(int id) {
+    this.id = id;
+  }
 
 }
